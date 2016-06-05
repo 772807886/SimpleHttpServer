3b0e4916e981d7e3e13e5c651136a1147d18e675
金黎明 <jinliming2@gmail.com>
2016-06-05 09:41:27 +0800

    窗口界面布局

--- SimpleHttpServer/Form1.cs
+++ SimpleHttpServer/Form1.cs
@@ -1,16 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace SimpleHttpServer {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            cbIpAddress.SelectedIndex = 0;
+        }
+
+        private void tbPort_KeyPress(object sender, KeyPressEventArgs e) {
+            if(!(char.IsNumber(e.KeyChar)) && e.KeyChar != (char)13 && e.KeyChar != (char)8) {
+                e.Handled = true;
+            }
+        }
+
+        private void tbPort_TextChanged(object sender, EventArgs e) {
+            string text = tbPort.Text;
+            if(tbPort.Text.Length == 0) {
+                tbPort.Text = "0";
+            } else if(int.Parse(tbPort.Text) > 65535) {
+                tbPort.Text = "65535";
+            } else {
+                tbPort.Text = Math.Abs(int.Parse(tbPort.Text)).ToString();
+            }
+            if(text != tbPort.Text) {
+                tbPort.SelectionStart = tbPort.Text.Length;
+            }
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e) {
+            if(folderBrowser.ShowDialog() == DialogResult.OK) {
+                tbFolder.Text = folderBrowser.SelectedPath;
+            }
         }
     }
 }
