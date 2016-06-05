90288aed4214eb6d07df3fb8f2aea6bfcaa46b3d
金黎明 <jinliming2@gmail.com>
2016-06-05 10:39:16 +0800

    HTTP服务

---
+++ SimpleHttpServer/HttpHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleHttpServer {
+    /// <summary>
+    /// HTTP请求处理线程
+    /// </summary>
+    class HttpHandler : Thread {
+        /// <summary>
+        /// TCP接受客户端
+        /// </summary>
+        private TcpClient client;
+        /// <summary>
+        /// 输入流
+        /// </summary>
+        private Stream input;
+        /// <summary>
+        /// 输出流
+        /// </summary>
+        private StreamWriter output;
+        /// <summary>
+        /// 字符串构造器
+        /// </summary>
+        private StringBuilder sb;
+        /// <summary>
+        /// HTTP请求处理
+        /// </summary>
+        /// <param name="client">TCP接受客户端</param>
+        public HttpHandler(TcpClient client) {
+            this.client = client;
+        }
+        /// <summary>
+        /// 执行
+        /// </summary>
+        public override void run() {
+            //输入输出流
+            input = new BufferedStream(client.GetStream());
+            output = new StreamWriter(new BufferedStream(client.GetStream()));
+            try {
+            } catch(Exception e) {
+            } finally {
+                input.Close();
+                output.Close();
+                client.GetStream().Close();
+            }
+            client.Close();
+        }
+        /// <summary>
+        /// 读入一行
+        /// </summary>
+        /// <returns>一行数据</returns>
+        private string readLine() {
+            char c;
+            sb = new StringBuilder();
+            while(true) {
+                c = (char)input.ReadByte();
+                if(c == '\n') {
+                    break;
+                } else if(c == '\r') {
+                    continue;
+                } else if(c == -1) {
+                    System.Threading.Thread.Sleep(1);
+                    continue;
+                }
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ SimpleHttpServer/HttpServer.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleHttpServer {
+    /// <summary>
+    /// HTTP服务
+    /// </summary>
+    class HttpServer {
+        /// <summary>
+        /// 监听地址及端口号
+        /// </summary>
+        private IPEndPoint server;
+        /// <summary>
+        /// 监听器
+        /// </summary>
+        TcpListener listener;
+        /// <summary>
+        /// HTTP服务
+        /// </summary>
+        /// <param name="ip_address">监听地址</param>
+        /// <param name="port">监听端口号</param>
+        public HttpServer(string ip_address, int port) {
+            this.server = new IPEndPoint(IPAddress.Parse(ip_address), port);
+        }
+        public void listen() {
+            listener = new TcpListener(server);
+            listener.Start();
+            while(true) {
+                TcpClient client = listener.AcceptTcpClient();
+            }
+        }
+    }
+}
