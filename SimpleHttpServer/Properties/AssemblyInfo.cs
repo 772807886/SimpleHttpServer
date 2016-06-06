b3a4cb128dcaf68e83f4b4736fadaa16a5664cca
金黎明 <jinliming2@gmail.com>
2016-06-06 14:43:43 +0800

    更新版本号

--- SimpleHttpServer/Properties/AssemblyInfo.cs
+++ SimpleHttpServer/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("SimpleHttpServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("一个简易的HTTP服务器")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Liming Jin")]
 [assembly: AssemblyProduct("SimpleHttpServer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Liming Jin")]
+[assembly: AssemblyTrademark("Sun Soft")]
 [assembly: AssemblyCulture("")]
 
 //将 ComVisible 设置为 false 将使此程序集中的类型
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
